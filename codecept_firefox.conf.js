217865db99224bee9c2b6de1f3f4a1ec547d3a4d
Al Meln <alsermel@gmail.com>
2020-03-21 18:14:33 +0300

    Добавил конфигурацию для Firefox, но запуск только через кривой override c https://codecept.io/configuration/#dynamic-configuration

---
+++ codecept_firefox.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://github.com',
+      show: true,
+      firefox: {
+        args: [
+          "--ignore-certificate-errors"
+        ]
+      }
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'my-auto-e2e-tests',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
